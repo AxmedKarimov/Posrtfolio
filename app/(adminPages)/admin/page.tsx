f81eca1e386fbea505fbbcbd807c2f898e5dc876
AxmedKarimov <karimovalijon383@gmail.com>
2025-03-27 12:36:55 +0500

    admin password

--- app/(adminPages)/admin/page.tsx
+++ app/(adminPages)/admin/page.tsx
@@ -1,50 +1,37 @@
 "use client";
-import Link from "next/link";
-import React from "react";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Admin() {
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const checkPassword = () => {
+    if (password === "menAdminman") {
+      router.push("/dashboard");
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
-    <div className="p-6  min-h-screen">
-      <h1 className="text-6xl mt-10 ms-10 text-white font-bold mb-10">
-        Hello, Admin
-      </h1>
-      <div className="max-w-5xl grid grid-cols-2 gap-6 items-center justify-center mx-auto">
-        <div className="w-full h-60 bg-neutral-800  p-6 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow">
-          <h1 className="text-white text-4xl font-semibold mb-4">
-            Asbob uskunalar
-          </h1>
-          <h1 className="text-white text-3xl font-semibold mb-4">2</h1>
-          <Link
-            href={"/addTools"}
-            className="mt-4 px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors"
-          >
-            Uskuna qo'shish
-          </Link>
-        </div>
-
-        <div className="w-full h-60 bg-neutral-800  p-6 rounded-2xl shadow-lg mb-4 hover:shadow-2xl transition-shadow">
-          <h1 className="text-white text-4xl font-semibold">Projectlar</h1>
-          <h1 className="text-white text-3xl font-semibold mb-4">2</h1>
-
-          <Link
-            href={"./addProject"}
-            className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
-          >
-            Project qo'shish
-          </Link>
-        </div>
-
-        <div className="w-full h-60 bg-neutral-800  p-6 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow">
-          <h1 className="text-white text-4xl font- mb-4">Mijozlar</h1>
-          <h1 className="text-white text-3xl font-semibold mb-4">2</h1>
-
-          <Link
-            href={"/addClient"}
-            className="mt-4 px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors"
-          >
-            Mijoz qo'shish
-          </Link>
-        </div>
+    <div className="h-screen w-full flex items-center justify-center ">
+      <div className="h-96 w-96 bg-gray-800 rounded-lg shadow-lg flex flex-col items-center justify-center p-6">
+        <h2 className="text-white text-2xl mb-4">Admin Panel</h2>
+        <input
+          type="password"
+          placeholder="Parolni kiriting"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="p-2 rounded bg-gray-700 text-white w-full mb-4"
+        />
+        <button
+          onClick={checkPassword}
+          className="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition"
+        >
+          Tekshirish
+        </button>
       </div>
     </div>
   );
